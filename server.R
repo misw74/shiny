63f8a63cdefe77109cb5a9b96237f6e6de0f39ff
Michal Switala <misw74@gmail.com>
2015-04-23 17:28:17 +0200

    no message

---
+++ server.R
@@ -0,0 +1,83 @@
+library(shiny)
+
+SIMULATED <- F
+thePi <- NULL
+
+is.inside <- function(x, y, r) {
+  myRetVal <- x^2 + y^2 <= r^2  
+  
+  myRetVal
+}
+
+getPi <- function(runs, r) {
+  myRetVal <- thePi
+    
+  if( !SIMULATED ) {    
+    withProgress(message = 'Simulating values', value = 0.1, {
+    set.seed(12345)
+  
+    myRetVal <- NULL
+    myRetVal$runs <- runs  
+    myRetVal$radius <- as.numeric(r)
+    
+    myRetVal$xs <- runif(runs,min= -myRetVal$radius ,max= myRetVal$radius )
+    myRetVal$ys <- runif(runs,min= -myRetVal$radius ,max= myRetVal$radius )
+    
+    myRetVal$pi <-  (sum(is.inside(myRetVal$xs,myRetVal$ys,myRetVal$radius))/runs) * 4
+    myRetVal$prc <- abs(100*(myRetVal$pi - pi)/pi)
+    myRetVal$diff <- myRetVal$pi - pi
+    
+    thePi <- myRetVal
+    SIMULATED <- T
+    })
+  }
+  
+  myRetVal
+}
+
+shinyServer(function(input, output) {
+  
+  output$pi <- renderText( 
+    {             
+      input$submit      
+      getPi( isolate(input$sims) * 1000, isolate(input$radius) )$pi
+    }     
+  )
+  
+  output$prc <- renderText( 
+    { 
+      input$submit
+      myPi <- getPi( isolate(input$sims) * 1000, isolate(input$radius) )
+      paste( round(myPi$prc,4), "%")  
+    }     
+  )
+  
+  output$diff <- renderText( 
+  {  
+    input$submit    
+    myPi <- getPi( isolate(input$sims) * 1000, isolate(input$radius) )
+  
+    round(myPi$diff,4)
+  }     
+  )
+  
+  output$piPlot <- renderPlot({        
+    input$submit
+
+    r <- 0
+    runs <- 0
+    
+    runs <- isolate(input$sims) * 1000
+    r <- as.numeric(isolate(input$radius))
+    
+    if( r > 0 ) {
+      myPi <- getPi( runs, r )
+    
+      withProgress(message = 'Generating plot', value = 0.1, {
+      plot(myPi$xs,myPi$ys,pch='.',col=ifelse( is.inside(myPi$xs,myPi$ys, myPi$radius),"blue","grey")
+           ,xlab='',ylab='',asp=1, xaxt='n',yaxt='n', axes = F,
+          main=paste(runs, "simulations, radius", myPi$radius))       
+      })
+    }
+  })
+})
---
+++ ui.R
@@ -0,0 +1,64 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  # Application title
+
+  titlePanel(withMathJax("\\(\\pi\\) approximation with Monte Carlo")),
+  
+  fluidRow(
+    column(5,
+           br(),
+           sliderInput("sims",
+                       "number of simulations ( x 1000 )",
+                       min = 25,
+                       max = 1000,
+                       value = 1),
+           selectInput("radius", c( "1", "2", "5", "10", "25"), label = "radius"),
+           actionButton("submit", "Simulate Again"),
+           br(),
+           br(),
+           fluidRow(
+             column(5,
+                    withMathJax( "\\(\\pi_{estimated}\\)")
+             ),
+             column(5,
+                    strong(textOutput("pi"))
+             )
+           ),           
+           fluidRow(
+             column(5,
+                    withMathJax("\\(\\pi - \\pi_{estimated}\\)" )
+             ),
+             column(5,
+                    textOutput("diff")
+             )
+           ),           
+           fluidRow(
+             column(5,
+                    "error"
+             ),
+             column(5,
+                    textOutput("prc")
+             )
+           )
+           
+    ),
+    column(7,
+           plotOutput("piPlot")
+    )    
+  ),  
+  br(),
+  fluidRow(
+  column(12,
+  withMathJax("The approximation is based on the fact that \\( A_{circle} = \\pi * r^2 \\) and \\( A_{square} = 4 * r^2 \\)" ),               
+  withMathJax( "what means that \\( \\pi = 4 * A_{circle}/A_{square} \\)" ),
+  withMathJax("Both areas (\\( A_{circle}, A_{square}\\) ) can be estimated by selecting random point on the square and checking if the point belongs to the circle or not by "),
+  withMathJax("looking at \\( x^2 + y^2 <= r^2 \\)"),  
+  br(),
+  br(),
+  withMathJax("The value of \\( \\pi \\) can be now estimated as follows"),
+  withMathJax("$$ \\pi_{estimated} = 4 * numberOfBluePoints / TotalNumberOfPoints $$")
+  )
+  )
+))
